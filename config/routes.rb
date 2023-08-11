7ea4252c9e20aaef404b0872c1a595eaaa6a6705
k-suke39 <pz.mozarla@gmail.com>
2023-08-11 21:01:04 +0900

    ⚡️

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'staticpages/top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "staticpages#top"
   # Defines the root path route ("/")
   # root "articles#index"
 end
